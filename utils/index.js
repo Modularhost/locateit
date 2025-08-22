d82f82577372e967790f5f9da4093bf1737401e7
Modularhost <host.modular@gmail.com>
2025-08-22 15:40:22 -0400

    Delete utils directory

--- utils/index.js
+++
@@ -1,5 +0,0 @@
-export const abc = (n) => Array.from({ length: n }, (_, i) => String.fromCharCode(65 + i));
-
-export const clamp = (v, min, max) => Math.max(min, Math.min(max, v));
-
-export const uid = () => Math.random().toString(36).slice(2, 10);
